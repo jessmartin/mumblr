aba36361883c4e3d412bb2c3280dd7ed20686e39
Jess Martin <jessmartin@gmail.com>
2022-05-05 17:21:54 -0400

    Adding wn auth

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,38 @@
-import './style.css'
+import "./style.css";
+import * as wn from "webnative";
+
+const state = await wn
+  .initialise({
+    permissions: {
+      app: {
+        name: "fumblr",
+        creator: "Jess Martin",
+      },
+      fs: {
+        private: [wn.path.directory("Posts", "Private")],
+        public: [wn.path.directory("Posts", "Public")],
+      },
+    },
+  })
+  .catch((err) => {
+    switch (err) {
+      case wn.InitialisationError.InsecureContext:
+      // TODO: Notify the user that the app is not secure
+      case wn.InitialisationError.UnsupportedBrowser:
+      // TODO: Notify the user of the error
+    }
+  });
+
+switch (state?.scenario) {
+  case wn.Scenario.AuthCancelled:
+    break;
+
+  case wn.Scenario.AuthSucceeded:
+  case wn.Scenario.Continuation:
+    state.fs;
+    break;
+
+  case wn.Scenario.NotAuthorised:
+    wn.redirectToLobby(state.permissions);
+    break;
+}
