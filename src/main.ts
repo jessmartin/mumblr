07b2d6ada1b9ed81435a6d6a16c83c26bd2729e8
Jess Martin <jessmartin@gmail.com>
2022-05-06 13:01:19 -0400

    More work

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import "./style.css";
 import * as wn from "webnative";
+import FileSystem from "webnative/fs/index";
 
 const state = await wn
   .initialise({
@@ -23,16 +24,32 @@ const state = await wn
     }
   });
 
+let fs: FileSystem | undefined;
+
 switch (state?.scenario) {
   case wn.Scenario.AuthCancelled:
     break;
 
   case wn.Scenario.AuthSucceeded:
   case wn.Scenario.Continuation:
-    state.fs;
+    // TODO: Maybe put some 'logged in' state at the top right?
+    fs = state.fs;
     break;
 
   case wn.Scenario.NotAuthorised:
     wn.redirectToLobby(state.permissions);
     break;
 }
+
+const post = document.querySelector<HTMLInputElement>("#post")!;
+
+post.addEventListener("click", function () {
+  console.log("attempting to save file");
+  const title = document.querySelector<HTMLInputElement>(".title-input")!;
+  const body = document.querySelector<HTMLTextAreaElement>(".body-input")!;
+  if (fs !== undefined && fs.appPath !== undefined) {
+    fs.add(`${fs.appPath()}/Posts/Public/${title.value}.md`, body.value).then(
+      console.log("saved file")
+    );
+  }
+});
