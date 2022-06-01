eb3cd865363820395123f309f73f61b0da5db5a6
Jess Martin <jessmartin@gmail.com>
2022-06-01 12:12:59 -0400

    Log in button

--- src/main.ts
+++ src/main.ts
@@ -2,77 +2,108 @@ import "./style.css";
 import * as wn from "webnative";
 import FileSystem from "webnative/fs/index";
 
-const state = await wn
-  .initialise({
-    permissions: {
-      app: {
-        name: "mumblr",
-        creator: "Jess Martin",
+// Check if the user is logged in.
+// TODO: Figure out how to do this!
+
+// If user is _not_ logged in, hide the interface.
+//   Currently, it starts out hidden. Nothing to do here.
+
+// If user is logged in, show the log in button.
+//   Currently, it starts out shown. Nothing to do here.
+
+async function main() {
+  const state = await wn
+    .initialise({
+      permissions: {
+        app: {
+          name: "mumblr",
+          creator: "Jess Martin",
+        },
+        fs: {
+          private: [wn.path.directory("Posts")], // This will be `private/Posts`
+          public: [wn.path.directory("Posts")], // This will be `public/Posts`
+        },
       },
-      fs: {
-        private: [wn.path.directory("Posts")], // This will be `private/Posts`
-        public: [wn.path.directory("Posts")], // This will be `public/Posts`
-      },
-    },
-  })
-  .catch((err) => {
-    switch (err) {
-      case wn.InitialisationError.InsecureContext:
-      // TODO: Notify the user that the app is not secure
-      case wn.InitialisationError.UnsupportedBrowser:
-      // TODO: Notify the user of the error
+    })
+    .catch((err) => {
+      switch (err) {
+        case wn.InitialisationError.InsecureContext:
+        // TODO: Notify the user that the app is not secure
+        case wn.InitialisationError.UnsupportedBrowser:
+        // TODO: Notify the user of the error
+      }
+    });
+
+  // Give me maximum debuggage
+  wn.setup.debug({ enabled: true });
+
+  let fs: FileSystem | undefined;
+
+  switch (state?.scenario) {
+    case wn.Scenario.AuthCancelled:
+      console.log("Auth cancelled by user");
+      break;
+
+    case wn.Scenario.AuthSucceeded:
+    case wn.Scenario.Continuation:
+      // TODO: Maybe put some 'logged in as' state at the top right?
+      console.log("Logged in");
+      fs = state.fs;
+      break;
+
+    case wn.Scenario.NotAuthorised:
+      wn.redirectToLobby(state.permissions);
+      console.log("Redirected to lobby");
+      break;
+  }
+
+  const post = document.querySelector<HTMLInputElement>("#post")!;
+
+  post.addEventListener("click", async function () {
+    console.log("attempting to save file");
+    const title = document.querySelector<HTMLInputElement>(".title-input")!;
+    // If title is blank, don't allow saving?
+
+    const body = document.querySelector<HTMLTextAreaElement>(".body-input")!;
+    // If body is blank, don't allow saving?
+
+    if (fs !== undefined) {
+      const filePath = wn.path.file("public", "Posts", `${title.value}.md`);
+      await fs.add(filePath, body.value).then(() => {
+        console.log("file saved");
+      });
+      await fs
+        .publish()
+        .then(() => {
+          console.log("file system published");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      console.log("no file system");
     }
   });
-
-// Give me maximum debuggage
-wn.setup.debug({ enabled: true });
-
-let fs: FileSystem | undefined;
-
-switch (state?.scenario) {
-  case wn.Scenario.AuthCancelled:
-    console.log("Auth cancelled by user");
-    break;
-
-  case wn.Scenario.AuthSucceeded:
-  case wn.Scenario.Continuation:
-    // TODO: Maybe put some 'logged in as' state at the top right?
-    console.log("Logged in");
-    fs = state.fs;
-    break;
-
-  case wn.Scenario.NotAuthorised:
-    wn.redirectToLobby(state.permissions);
-    console.log("Redirected to lobby");
-    break;
 }
 
-const post = document.querySelector<HTMLInputElement>("#post")!;
+const logIn = document.querySelector<HTMLInputElement>("#log-in")!;
+logIn.addEventListener("click", async function () {
+  // Initialize WebNative (which will force a log-in)
+  // TODO: Why does this not finish before the items below are hidden?
+  await main();
 
-post.addEventListener("click", async function () {
-  console.log("attempting to save file");
-  const title = document.querySelector<HTMLInputElement>(".title-input")!;
-  // If title is blank, don't allow saving?
+  // Show the interface!
+  const logOut = document.querySelector<HTMLInputElement>("#log-out")!;
+  logOut.style.display = "block";
+  const titleInput = document.querySelector<HTMLInputElement>(".title-input")!;
+  titleInput.style.display = "block";
+  const bodyInput = document.querySelector<HTMLInputElement>(".body-input")!;
+  bodyInput.style.display = "block";
+  const postButton = document.querySelector<HTMLInputElement>(".post-button")!;
+  postButton.style.display = "block";
 
-  const body = document.querySelector<HTMLTextAreaElement>(".body-input")!;
-  // If body is blank, don't allow saving?
-
-  if (fs !== undefined) {
-    const filePath = wn.path.file("public", "Posts", `${title.value}.md`);
-    await fs.add(filePath, body.value).then(() => {
-      console.log("file saved");
-    });
-    await fs
-      .publish()
-      .then(() => {
-        console.log("file system published");
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  } else {
-    console.log("no file system");
-  }
+  // Hide the log in button
+  logIn.style.display = "none";
 });
 
 const logOut = document.querySelector<HTMLInputElement>("#log-out")!;
