3642a3e65d5b0f2d954907dbdfd20525b95e5ff8
Jess Martin <jessmartin@gmail.com>
2022-06-15 13:16:11 -0400

    Read markdown files from WebNative, parse with remark

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import "./style.css";
 import * as wn from "webnative";
 import FileSystem from "webnative/fs/index";
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import rehypeStringify from "rehype-stringify";
+import remarkFrontmatter from "remark-frontmatter";
+import extract from "remark-extract-frontmatter";
+import { parse } from "yaml";
 
 const permissions = {
   app: {
@@ -97,4 +104,59 @@ post.addEventListener("click", async function () {
   } else {
     console.log("no file system");
   }
+});
+
+const buildSiteButton =
+  document.querySelector<HTMLInputElement>("#build-button")!;
+
+buildSiteButton.addEventListener("click", async function () {
+  // Read the most recent Markdown files
+  let markdownPosts = [];
+  if (fs !== undefined) {
+    const linksObject = await fs.ls(wn.path.directory("public", "Posts"));
+    const links = Object.entries(linksObject);
+    const posts = await Promise.all(
+      links.map(([name, _]) => {
+        const filePath = wn.path.file("public", "Posts", name);
+        return fs?.cat(filePath);
+      })
+    );
+
+    // Convert the FileContentRaw, which is a uint8array to string
+    const filesContents = [];
+    const decoder = new TextDecoder();
+    for (const post of posts) {
+      filesContents.push(decoder.decode(post as Uint8Array));
+    }
+
+    // Convert the string to micromark
+    const parsedPosts = await Promise.all(
+      filesContents.map((content) => {
+        return unified()
+          .use(remarkParse)
+          .use(remarkFrontmatter)
+          .use(extract, { yaml: parse })
+          .use(remarkRehype)
+          .use(rehypeStringify)
+          .process(content);
+      })
+    );
+
+    // Ignore files that don't have postedAt
+    markdownPosts = parsedPosts.filter((post) => {
+      return post.data.postedAt !== undefined;
+    });
+
+    // Sort the files by postedAt
+    markdownPosts.sort((a, b) => {
+      return (
+        new Date(b.data.postedAt as string).getTime() -
+        new Date(a.data.postedAt as string).getTime()
+      );
+    });
+  }
+  // Build the HTML/CSS
+
+  // Write the static site to IPFS
+  // Show a simple IPFS Web Gateway URL where the site can be viewed
 });
